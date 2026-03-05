de087da2ae41d7d3d7175f5a17eec2aef88fb348
Dmitriy <flezokt@gmail.com>
2026-03-05 20:49:04 +0400

    Получение значений инпутов

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btnAdd = document.querySelector(".btn-add");
+const contactName = document.querySelector(".input-name");
+const vacancy = document.querySelector(".input-vacancy");
+const phone = document.querySelector(".input-phone");
+
+const contactDB = [];
+
+btnAdd.addEventListener('click', () => {
+    
+});
+
+
