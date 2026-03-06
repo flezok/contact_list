473f5c634e718d73f81a780a52a0d79658dc70e9
Dmitriy <flezokt@gmail.com>
2026-03-07 03:51:43 +0400

    Разработка структуры данных, сохранение контактов, отображение и сортировка контактов по буквам, открытие и закрытие списка контактов у буквы

--- js/main.js
+++ js/main.js
@@ -1,12 +1,83 @@
-const btnAdd = document.querySelector(".btn-add");
-const contactName = document.querySelector(".input-name");
-const vacancy = document.querySelector(".input-vacancy");
-const phone = document.querySelector(".input-phone");
+window.onload = function () {
+    const btnAdd = document.querySelector(".btn-add");
+    const tableItem = document.getElementsByClassName("contact-table__item");
 
-const contactDB = [];
+    const contactDB = {
+        contacts: [],
 
-btnAdd.addEventListener('click', () => {
-    
-});
+        createContactDB() {
+
+            const contact = {
+                name: document.querySelector(".input-name").value,
+                vacancy: document.querySelector(".input-vacancy").value,
+                phone: document.querySelector(".input-phone").value,
+                created: false
+            };
+
+            this.contacts.push(contact);
+            return contact;
+        }
+
+    };
+
+    function createContact(contact) {
+        return `
+        <div class="contacts-item">
+            <div class="contacts-item__inner">
+                <p class="contacts-item__text">
+                    Name: ${contact.name}
+                </p>
+                <p class="contacts-item__text">
+                    Vacancy: ${contact.vacancy}
+                </p>
+                <p class="contacts-item__text">
+                    Phone: ${contact.phone}
+                </p>
+            </div>
+            <button class="contacts-item-delete">
+                x
+            </button>
+        </div>
+        `
+    }
+
+    btnAdd.addEventListener('click', () => {
 
 
+        contactDB.createContactDB()
+
+
+        contactDB.contacts.forEach((e) => {
+            [...tableItem].forEach((i) => {
+                if (e.name[0] === i.dataset.id && e.created === false) {
+                    let thisContacts = i.querySelector('.contact-table__contacts');
+
+                    thisContacts.insertAdjacentHTML('beforeend', createContact(e));
+
+                    let count = thisContacts.childElementCount;
+
+
+                    i.querySelector('.contact-table__inner .table__item-count').textContent = count;
+
+                    e.created = true;
+                }
+            })
+        });
+
+    });
+
+   [...tableItem].forEach(i => {
+    i.addEventListener('click', () => {
+        const count = i.querySelector('.table__item-count');
+
+        const contacts = i.querySelector('.contact-table__contacts');
+
+        if (count.textContent.trim().length > 0 && !i.classList.contains('contact-table__contacts--active')) {
+            i.classList.add('contact-table__contacts--active');
+        } else if (count.textContent.trim().length > 0 && i.classList.contains('contact-table__contacts--active')) {
+            i.classList.remove('contact-table__contacts--active');
+        }
+    })
+   })
+
+}
